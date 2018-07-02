2ac069f13d08004dd1e5e233b4eca8a5f3fd4eda
monycky <monyckyfrazao@gmail.com>
2018-07-02 13:42:42 -0300

    fix: purschased

--- data_preprocessing_template.R
+++ data_preprocessing_template.R
@@ -15,6 +15,6 @@ dataset$Salary = ifelse(is.na(dataset$Salary),
 dataset$Country = factor(dataset$Country,
                          levels = c('France', 'Spain', 'Germany'),
                          labels = c(1, 2, 3))
-dataset$Purcharsed = factor(dataset$Purcharsed,
+dataset$Purchased = factor(dataset$Purchased,
                          levels = c('No', 'Yes'),
                          labels = c(0, 1))
