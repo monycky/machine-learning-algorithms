4218a17e164d3e79a968577b09a3c56504746b1f
monycky <monyckyfrazao@gmail.com>
2018-07-02 14:57:05 -0300

    add: feature scaling in X train and test

--- data_preprocessing_template.py
+++ data_preprocessing_template.py
@@ -32,3 +32,9 @@ y = labelenconder_y.fit_transform(y)
 #Splitting the dataset into the Training set and Test set
 from sklearn.model_selection import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0) 
+
+#Feature Scaling
+from sklearn.preprocessing import StandardScaler
+sc_X = StandardScaler()
+X_train = sc_X.fit_transform(X_train)
+X_test = sc_X.transform(X_test)
