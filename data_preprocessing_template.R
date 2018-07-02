efae36672288eef0b62cb23455c86d59e2a821f7
monycky <monyckyfrazao@gmail.com>
2018-07-02 09:50:58 -0300

    add: getting age data missing

--- data_preprocessing_template.R
+++ data_preprocessing_template.R
@@ -1,2 +1,9 @@
+#Data Preprocessing
+
 #Importing DataSet
 dataset = read.csv('Data.csv')
+
+#Taking care of missing data
+dataset$Age = ifelse(is.na(dataset$Age),
+                     ave(dataset$Age, FUN = function(x) mean(x, na.rm = TRUE)),
+                     dataset$Age)
