e79b6a6bc60833b14c2a45f2a69730c80e924979
monycky <monyckyfrazao@gmail.com>
2018-07-02 10:50:01 -0300

    add: treating category labels

--- data_preprocessing_template.py
+++ data_preprocessing_template.py
@@ -17,3 +17,11 @@ from sklearn.preprocessing import Imputer
 imputer = Imputer(missing_values = 'NaN', strategy = 'mean', axis = 0)
 imputer = imputer.fit(X[:,  1:3])
 X[:, 1:3] = imputer.transform(X[:, 1:3])
+
+#Enconding categorical data
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+labelenconder_X = LabelEncoder()
+X[:, 0] = labelenconder_X.fit_transform(X[:, 0])
+
+oneHotEncoder = OneHotEncoder(categorical_features = [0])
+X = oneHotEncoder.fit_transform(X).toarray()
