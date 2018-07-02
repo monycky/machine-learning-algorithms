147f3cd12adb13fe1541964f204176c9900e6c31
monycky <monyckyfrazao@gmail.com>
2018-07-02 14:03:40 -0300

    add: split and train data

--- data_preprocessing_template.py
+++ data_preprocessing_template.py
@@ -28,3 +28,7 @@ X = oneHotEncoder.fit_transform(X).toarray()
 
 labelenconder_y = LabelEncoder()
 y = labelenconder_y.fit_transform(y)
+
+#Splitting the dataset into the Training set and Test set
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0) 
