01b506d19b2e2aae29b48fbd4d5112b549d47ca8
monycky <monyckyfrazao@gmail.com>
2018-07-02 14:28:29 -0300

    add: split and train dataset in R

--- data_preprocessing_template.R
+++ data_preprocessing_template.R
@@ -18,3 +18,11 @@ dataset$Country = factor(dataset$Country,
 dataset$Purchased = factor(dataset$Purchased,
                          levels = c('No', 'Yes'),
                          labels = c(0, 1))
+
+#Splitting the dataser into the training ser and Test set
+#install.packages('caTools')
+library(caTools)
+set.seed(123)
+split = sample.split(dataset$Purchased, SplitRatio = 0.8)
+training_set = subset(dataset, split == TRUE)
+test_set = subset(dataset, split == FALSE)
