c5d4a2d899fe92f23e25bbe3998930196bb93bbf
monycky <monyckyfrazao@gmail.com>
2018-07-02 13:21:44 -0300

    add: encoding categorical data

--- data_preprocessing_template.R
+++ data_preprocessing_template.R
@@ -10,3 +10,9 @@ dataset$Age = ifelse(is.na(dataset$Age),
 dataset$Salary = ifelse(is.na(dataset$Salary),
                      ave(dataset$Salary, FUN = function(x) mean(x, na.rm = TRUE)),
                      dataset$Salary)
+
+#Encoding categorical data
+dataset$Country = factor(dataset$Country,
+                         levels = c('France', 'Spain', 'Germany'),
+                         labels = c(1, 2, 3))
+
