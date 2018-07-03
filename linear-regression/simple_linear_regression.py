14ca626657c52c953fa161d16aad09494bde8e7a
monycky <monyckyfrazao@gmail.com>
2018-07-03 09:15:35 -0300

    add: fit and set simple linear regression to th train

--- linear-regression/simple_linear_regression.py
+++ linear-regression/simple_linear_regression.py
@@ -12,10 +12,15 @@ y = dataset.iloc[:, 1].values
 
 #Splitting the dataset into the Training set and Test set
 from sklearn.model_selection import train_test_split
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0) 
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 1/3, random_state = 0) 
 
 #Feature Scaling
 '''from sklearn.preprocessing import StandardScaler
 sc_X = StandardScaler()
 X_train = sc_X.fit_transform(X_train)
 X_test = sc_X.transform(X_test)'''
+
+#Fitting Simple Linear Regression to the Training test
+from sklearn.linear_model import LinearRegression
+regressor = LinearRegression()
+regressor.fit(X_train, y_train )
