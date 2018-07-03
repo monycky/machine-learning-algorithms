02e6054f945667099f934e3a71183ffe8fc523be
monycky <monyckyfrazao@gmail.com>
2018-07-03 10:15:23 -0300

    add: set template to simple linear regression on R

---
+++ linear-regression/simple_linear_regression.R
@@ -0,0 +1,15 @@
+#Simple Linear Regression
+
+dataset = read.csv('Salary_Data.csv')
+
+#Splitting the dataser into the training ser and Test set
+#install.packages('caTools')
+library(caTools)
+set.seed(123)
+split = sample.split(dataset$Salary, SplitRatio = 2/3)
+training_set = subset(dataset, split == TRUE)
+test_set = subset(dataset, split == FALSE)
+
+#Feature Scaling
+#training_set[, 2:3] = scale(training_set[, 2:3])
+#test_set[, 2:3] = scale(test_set[, 2:3])
