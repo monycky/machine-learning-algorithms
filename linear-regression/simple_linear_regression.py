2f1ffc7e5156ef169454a56ebfa5a5ce51c3b7db
monycky <monyckyfrazao@gmail.com>
2018-07-03 09:58:56 -0300

    add: visualising train and test results

--- linear-regression/simple_linear_regression.py
+++ linear-regression/simple_linear_regression.py
@@ -23,7 +23,23 @@ X_test = sc_X.transform(X_test)'''
 #Fitting Simple Linear Regression to the Training test
 from sklearn.linear_model import LinearRegression
 regressor = LinearRegression()
-regressor.fit(X_train, y_train )
+regressor.fit(X_train, y_train)
 
 #Predicting the Test set results
 y_pred = regressor.predict(X_test)
+
+#Visualising the Training set results
+plt.scatter(X_train, y_train, color = 'red')
+plt.plot(X_train, regressor.predict(X_train), color = 'blue')
+plt.title('Salary vs Exprerience (Training set)')
+plt.xlabel('Years of Experience')
+plt.ylabel('Salary')
+plt.show()
+
+#Visualising the Test set results
+plt.scatter(X_test, y_test, color = 'red')
+plt.plot(X_train, regressor.predict(X_train), color = 'blue')
+plt.title('Salary vs Exprerience (Test set)')
+plt.xlabel('Years of Experience')
+plt.ylabel('Salary')
+plt.show()
