1963592e3a42141d64a9435ebe131110129d12f7
monycky <monyckyfrazao@gmail.com>
2018-07-03 16:59:15 -0300

    add: enconding categorical data

--- multiple-linear-regression/multiple_linear_regression.py
+++ multiple-linear-regression/multiple_linear_regression.py
@@ -10,6 +10,16 @@ dataset = pd.read_csv('50_Startups.csv')
 X = dataset.iloc[:, :-1].values
 y = dataset.iloc[:, 4].values
 
+# Encoding categorical data
+# Encoding the Independent Variable
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+labelencoder_X = LabelEncoder()
+X[:, 0] = labelencoder_X.fit_transform(X[:, 0])
+onehotencoder = OneHotEncoder(categorical_features = [0])
+X = onehotencoder.fit_transform(X).toarray()
+# Encoding the Dependent Variable
+labelencoder_y = LabelEncoder()
+y = labelencoder_y.fit_transform(y)
 
 #Splitting the dataset into the Training set and Test set
 from sklearn.model_selection import train_test_split
