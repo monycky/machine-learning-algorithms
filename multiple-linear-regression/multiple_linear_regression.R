e0e4fe2d5190d33359a64ad74a1e6e9f62ff7b8b
monycky <monyckyfrazao@gmail.com>
2018-07-04 16:59:11 -0300

    add: multiple-linear-regression templante in R

---
+++ multiple-linear-regression/multiple_linear_regression.R
@@ -0,0 +1,23 @@
+#Multiple Linear Regression
+
+dataset = read.csv('50_Startups.csv')
+#dataset = dataset[, 2:3]
+
+# Encoding categorical data
+dataset$State = factor(dataset$State,
+                       levels = c('New York', 'California', 'Florida'),
+                       labels = c(1, 2, 3))
+
+
+#Splitting the dataser into the training ser and Test set
+#install.packages('caTools')
+library(caTools)
+set.seed(123)
+split = sample.split(dataset$Profit, SplitRatio = 0.8)
+training_set = subset(dataset, split == TRUE)
+test_set = subset(dataset, split == FALSE)
+
+#Feature Scaling
+#training_set[, 2:3] = scale(training_set[, 2:3])
+#test_set[, 2:3] = scale(test_set[, 2:3])
+
