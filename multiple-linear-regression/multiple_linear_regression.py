e818b2eb2df0b42ffbc9492d64d7b54e7aae0577
monycky <monyckyfrazao@gmail.com>
2018-07-04 16:30:49 -0300

    add: running backward elimination

--- multiple-linear-regression/multiple_linear_regression.py
+++ multiple-linear-regression/multiple_linear_regression.py
@@ -42,3 +42,19 @@ y_pred = regressor.predict(X_test)
 #Building the optimal model Backward Elimination
 import statsmodels.formula.api as sm
 X = np.append(arr = np.ones((50, 1)).astype(int), values = X, axis = 1)
+X_opt = X[:, [0, 1, 2, 3, 4, 5]]
+regressor_OLS = sm.OLS(endog = y, exog = X_opt).fit()
+regressor_OLS.summary()
+
+X_opt = X[:, [0, 1, 3, 4, 5]]
+regressor_OLS = sm.OLS(endog = y, exog = X_opt).fit()
+regressor_OLS.summary()
+
+X_opt = X[:, [0, 1, 3, 5]]
+regressor_OLS = sm.OLS(endog = y, exog = X_opt).fit()
+regressor_OLS.summary()
+
+X_opt = X[:, [0, 5]]
+regressor_OLS = sm.OLS(endog = y, exog = X_opt).fit()
+regressor_OLS.summary()
+
