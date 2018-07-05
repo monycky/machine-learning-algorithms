d8245c714d45275cf56a0a1370ad55934d81b3f2
monycky <monyckyfrazao@gmail.com>
2018-07-05 13:59:16 -0300

    add: predict results with linear and polynomial regression

--- polynomial-regression/polynomial_regression.py
+++ polynomial-regression/polynomial_regression.py
@@ -52,3 +52,8 @@ plt.xlabel('Position Level')
 plt.ylabel('Salary')
 plt.show()
 
+#Predict a new result with Linear Regression
+lin_reg.predict(6.5)
+
+#Predicting a new result with Polynomial Regression
+lin_reg_2.predict(poly_reg.fit_transform(6.5))
