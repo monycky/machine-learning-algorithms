a948bcc84b51a1781ee103523e7a023e1bba37f9
monycky <monyckyfrazao@gmail.com>
2018-07-05 10:15:49 -0300

    add: import and ajust dataset

--- polynomial-regression/polynomial_regression.py
+++ polynomial-regression/polynomial_regression.py
@@ -1,1 +1,22 @@
 #Polynomial Regression
+
+#Import the libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+#Import the Dataset
+dataset = pd.read_csv('Position_Salaries.csv')
+X = dataset.iloc[:, 1:2].values
+y = dataset.iloc[:, 2].values
+
+
+#Splitting the dataset into the Training set and Test set
+'''from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0) '''
+
+#Feature Scaling
+'''from sklearn.preprocessing import StandardScaler
+sc_X = StandardScaler()
+X_train = sc_X.fit_transform(X_train)
+X_test = sc_X.transform(X_test)'''
