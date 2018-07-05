ad253250105e476420417d71a878a42873be8aaa
monycky <monyckyfrazao@gmail.com>
2018-07-05 13:28:56 -0300

    add: visual polynomial regression results

--- polynomial-regression/polynomial_regression.py
+++ polynomial-regression/polynomial_regression.py
@@ -28,7 +28,7 @@ lin_reg.fit(X, y)
 
 #Fitting Polynomial Regression to the dataset
 from sklearn.preprocessing import PolynomialFeatures
-poly_reg = PolynomialFeatures(degree = 2)
+poly_reg = PolynomialFeatures(degree = 4)
 X_poly = poly_reg.fit_transform(X) 
 
 lin_reg_2 = LinearRegression()
@@ -38,6 +38,16 @@ lin_reg_2.fit(X_poly, y)
 plt.scatter(X, y, color = 'red')
 plt.plot(X, lin_reg.predict(X), color = 'blue')
 plt.title('Truth or Bluff (Linear Regression)')
+plt.xlabel('Position Level')
+plt.ylabel('Salary')
+plt.show()
+
+#Visualising the Polynomial Regression results
+X_grid = np.arange(min(X), max(X), 0.1)
+X_grid = X_grid.reshape((len(X_grid), 1))
+plt.scatter(X, y, color = 'red')
+plt.plot(X_grid, lin_reg_2.predict(poly_reg.fit_transform(X_grid)), color = 'blue')
+plt.title('Truth or Bluff (Polynomial Regression)')
 plt.xlabel('Position Level')
 plt.ylabel('Salary')
 plt.show()
