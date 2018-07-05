ca2b3f6238ad7a18b742e61ad1ae6a97d3d3848e
monycky <monyckyfrazao@gmail.com>
2018-07-05 10:54:02 -0300

    add: fit polynomial regression to the set

--- polynomial-regression/polynomial_regression.py
+++ polynomial-regression/polynomial_regression.py
@@ -25,3 +25,11 @@ X_test = sc_X.transform(X_test)'''
 from sklearn.linear_model import LinearRegression
 lin_reg = LinearRegression()
 lin_reg.fit(X, y)
+
+#Fitting Polynomial Regression to the dataset
+from sklearn.preprocessing import PolynomialFeatures
+poly_reg = PolynomialFeatures(degree = 2)
+X_poly = poly_reg.fit_transform(X) 
+
+lin_reg_2 = LinearRegression()
+lin_reg_2.fit(X_poly, y)
