27ef1aee348f05405f4d6e56b9609bf9674735cd
monycky <monyckyfrazao@gmail.com>
2018-07-06 09:27:34 -0300

    add: visual linear regression results

--- polynomial-regression/polynomial_regression.R
+++ polynomial-regression/polynomial_regression.R
@@ -20,3 +20,40 @@ dataset = dataset[2:3]
 #Fitting Linear Regression to the dataset
 lin_reg = lm(formula = Salary ~.,
              data = dataset)
+
+#Fitting Polynomial Regression to the dataset
+dataset$Level2 = dataset$Level^2
+dataset$Level3 = dataset$Level^3
+poly_reg = lm(formula = Salary ~.,
+              data = dataset)
+
+#Visualising the Linear Regression results
+#install.packages('ggplot2')
+library(ggplot2)
+ggplot() +
+  geom_point(aes(x = dataset$Level, y = dataset$Salary),
+             color = 'red') +
+  geom_line(aes(x = dataset$Level, y = predict(lin_reg, newdata = dataset)),
+            color = 'blue') +
+  ggtitle('Truth ou Bluff (Linear Regression')
+  xlab('Level')
+  ylab('Salary')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
