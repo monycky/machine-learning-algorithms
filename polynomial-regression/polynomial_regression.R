013685b46130806d276397fd82ebe44f8cfa1fea
monycky <monyckyfrazao@gmail.com>
2018-07-06 11:16:14 -0300

    add: prediciting a new results with linear and polynomial regression

--- polynomial-regression/polynomial_regression.R
+++ polynomial-regression/polynomial_regression.R
@@ -51,7 +51,15 @@ ggplot() +
   xlab('Level')
   ylab('Salary')  
 
-
+#Predicting a new results with Linear Regression
+y_pred = predict(lin_reg, data.frame(Level = 6.5))
+  
+  
+#Predicting a new results with Polynomial Regression
+y_pred = predict(poly_reg, data.frame(Level = 6.5,
+                                       Level2= 6.5^2,
+                                       Level3 = 6.5^3,
+                                       Level4 = 6.5^4))
 
 
 
